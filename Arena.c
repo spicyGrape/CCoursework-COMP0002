173f27bd13045be97db47d01eff9f6e329fe722e
Shuaiting Li <li.private0@gmail.com>
2024-10-24 15:21:59 +0100

    Separate the graphic procession from Arena.c

--- Arena.c
+++ Arena.c
@@ -74,54 +74,10 @@ void initMap()
     return;
 }
 
-void drawMap()
-{
-    for (int height = 0; height < ARENA_HEIGHT; height++)
-    {
-        for (int width = 0; width < ARENA_WIDTH; width++)
-        {
-            if (arenaMap[height][width] == 'B')
-            {
-                setColour(red);
-                fillRect(width * TILE_WIDTH, height * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH);
-            }
-            else if (arenaMap[height][width] == 'M')
-            {
-                setColour(gray);
-                fillRect(width * TILE_WIDTH, height * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH);
-            }
-            else if (arenaMap[height][width] == 'O')
-            {
-                setColour(black);
-                fillRect(width * TILE_WIDTH, height * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH);
-            }
-        }
-    }
-    return;
-}
-
-void drawGrid()
-{
-    setColour(gray);
-    // Vertical lines
-    for (int i = 0; i < ARENA_HEIGHT; i++)
-    {
-        drawLine(0, i * TILE_WIDTH, ARENA_WIDTH * TILE_WIDTH, i * TILE_WIDTH);
-    }
-    // Horizontal lines
-    for (int i = 0; i < ARENA_WIDTH; i++)
-    {
-        drawLine(i * TILE_WIDTH, 0, i * TILE_WIDTH, ARENA_HEIGHT * TILE_WIDTH);
-    }
-    return;
-}
-
 void setupArena()
 {
-    background();
     initWindow();
     initMap();
-    drawMap();
 }
 int main()
 {
---
+++ View.h
@@ -0,0 +1,6 @@
+#ifndef VIEW_H
+#define VIEW_H
+#include "graphics.h"
+#include "Arena.h"
+void drawMap();
+#endif
